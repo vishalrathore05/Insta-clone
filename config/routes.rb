2ffa5f4ac3bf3be7128f94c5657bd1149d80ab80
vishal singh rathore <vishalsinghrathore@vishals-MacBook-Air.local>
2023-10-30 01:55:34 -0700

    Add post and comment section

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,4 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # resources :users do
+  #   resources :posts do
+  #     resources :comments
+  #   end
+  # end
 end
